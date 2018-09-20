a2e2374fdf02974952a5ebb168fe4d8db9134998
seanreed1111 <seanreed1111@gmail.com>
2018-09-19 20:26:46 -0400

    adjusts functions-and-methods.scala

--- mystuff/functions-and-methods.scala
+++ mystuff/functions-and-methods.scala
@@ -1,7 +1,7 @@
 // partially applied functions aka curried functions
 
-def plus(a: Int)(b: Int) = a + b
-
+def plus(a: Int)(b: Int) = a + b // plus (Int, Int) => Int
+plus(4)(10)
 def plus2 = plus(2)(_) // plus2: Int => Int
 
 plus2(5) // Int = 7
@@ -9,6 +9,8 @@ plus2(5) // Int = 7
 
 
 def add(x: Int, y: Int) = x + y
+
+add(4,10)
 
 val addCurried = (add _).curried
 
